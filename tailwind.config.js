90d23b185382f3c569bd0484e1df71819a362537
avipj <avinashprajapati914@gmail.com>
2022-07-18 12:13:26 +0530

    🔧 configured tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
+      colors: {
+        primary: "#010101",
+      },
+
+      fontFamily: {
+        jost: ["Jost", "sans-serif"],
+        sen: ["Sen", "sans-serif"],
+      },
+
+      screens: {
+        custom: "400px",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
