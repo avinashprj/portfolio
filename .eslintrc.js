3668d49fa87143f09759686f7b350a9c60400c51
avipj <avinashprajapati914@gmail.com>
2022-07-18 20:51:05 +0530

    🚨 fixed eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,17 @@
-module.exports= {
-  "extends": ["wesbos/typescript"],
-   "rules": {
-    "@typescript-eslint/no-unsafe-assignment": false
+module.exports = {
+  extends: ["wesbos/typescript"],
+  rules: {
+    "react/prop-types": 0,
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        extendDefaults: true,
+        types: {
+          "{}": false,
+        },
+      },
+    ],
   },
-  "ignorePatterns": [".eslintrc.js","./tailwind.config.js"]
-}
+  ignorePatterns: [".eslintrc.js", "./tailwind.config.js"],
+};
