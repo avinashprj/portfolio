6d3568857ef9a63cd7b7c4ae4a8e2163dbb08886
avipj <avinashprajapati914@gmail.com>
2022-07-18 12:14:02 +0530

    ✨ started with header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { NextComponentType, NextPageContext } from "next";
+
+export const Header: NextComponentType = () => {
+  return (
+    <header>
+      <nav className="font-sen py-8 text-4xl text-gray-200">Anurag</nav>
+    </header>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@fontsource/jost/400.css';
+import '@fontsource/jost/500.css';
+import '@fontsource/jost/600.css';
+import '@fontsource/jost/700.css';
+import '@fontsource/sen/400.css';
+import '@fontsource/sen/700.css';
+
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextSeo
+        title="Avinash | Frontend Developer"
+        titleTemplate="Avinash | Frontend Developer"
+        defaultTitle="Avinash | Frontend Developer"
+        description="Hey! I'm Avinash, A Frontend Developer and a Student!"
+        twitter={{
+          handle: '@avinash_prj',
+          site: '@avinash_prj',
+          cardType: 'summary_large_image',
+        }}
+        additionalMetaTags={[
+          {
+            property: 'keywords',
+            content:
+              'Frontend Developer, avinash, avinashprj,avinash prajapati, Web Developer, web development, web developer, blogger, tech enthusiast, open source',
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
