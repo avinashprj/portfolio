f8cf09d8860d263f7191370cecf9e77056b6d7dd
avipj <avinashprajapati914@gmail.com>
2022-07-18 12:10:06 +0530

    🔥 changed some files

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports= {
+  "extends": ["wesbos/typescript"],
+   "rules": {
+    "@typescript-eslint/no-unsafe-assignment": false
+  },
+  "ignorePatterns": [".eslintrc.js","./tailwind.config.js"]
+}
