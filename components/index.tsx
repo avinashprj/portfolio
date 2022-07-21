7031b03ef15ba08cda85afbb18feb61bd302405d
avipj <avinashprajapati914@gmail.com>
2022-07-21 20:03:19 +0530

    ✨ added Skills component

--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from './Header/Header';
 import { TextLink } from './TextLinks/TextLinks';
 import { About } from './About/About';
+import { Skills } from './Skills/Skills';
 
-export { Header, TextLink, About };
+export { Header, TextLink, About, Skills };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
-import { About, Header } from '../components';
+import { About, Header, Skills } from '../components';
 
 const Home: NextPage = () => (
   <div className="px-2 sm:px-8 md:px-24 lg:px-48 xl:px-72">
     <Header />
     <main>
       <About />
+      <Skills />
     </main>
   </div>
 );
